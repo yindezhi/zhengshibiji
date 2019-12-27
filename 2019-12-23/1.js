f89b11860917ccb16823dcb39622fa9508d5e567
yindezhi <1273450467@qq.com>
2019-12-27 09:06:14 +0800

    001

--- 2019-12-23/1.js
+++ 2019-12-23/1.js
@@ -1,3 +1,21 @@
 /* 
     通过全局对象的__filename能够获取当前文件的绝对路径
 */
+// console.log(__filename);
+
+/* 
+    表示当前执行脚本所在的目录（运行文件的根目录），不包括文件本身
+*/
+
+// console.log(__dirname);
+
+let path = require('path');//找路径
+
+//console.log(path);//拼路径的，写啥拼啥
+console.log(path.join('wwwroot','./api.js'));
+//console.log(path.resolve('./','./2_server.js'))
+
+//console.log(path.resolve('wwwroot','./tmp/file/'));//最后一个参数要加./,要不然出不来
+
+//自动帮我们找当前文件的路径和配置路径进行链接
+// console.log(path.resolve(__dirname,'./1.js'));
